e08e88919284633c40ed7d48e048bcd7ca3ef98c
Alisson10101010 <alisson.teixeira@iesb.edu.br>
2025-03-20 21:35:00 -0300

    Aula 20/03

---
+++ projet-10/components/PrimeiroComponente.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import {statusBar} from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native';
+import PrimeiroComponente from './SegundoComponente
+
+export default function PrimeiroComponente () {
+
+
+
+return (
+    (View)
+    (Text)
+    Meu primeiro componente
+</Text>
+</View>
+);
+}
---
+++ projet-10/components/SegundoComponente.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import {} from 'react-native'
+
+function SegundoComponente () {
+    return <Text>Segundo Componente</Text>
+}
