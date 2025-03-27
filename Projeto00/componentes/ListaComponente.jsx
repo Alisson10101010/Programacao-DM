cef1f44f5f0bd9fd15527a8451a11cb81b09ec6d
Alisson10101010 <alisson.teixeira@iesb.edu.br>
2025-03-26 21:45:14 -0300

    Aula fazendo o projeto no cell

---
+++ Projeto00/componentes/ListaComponente.jsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from 'react';
+
+export default function Perfil(props) {
+    console.log("Propriedades ->", props);
+
+    return (
+        <View style={styles.container}>
+            {/* Conteúdo aqui */}
+        </View>
+    );
+}
+
---
+++ Projeto00/componentes/Perfil.jsx
@@ -0,0 +1,11 @@
+import { StyleSheet,Text, View } from "react-native";
+import React
+
+export default function Perfil (props) {
+
+    console.log("Propriedades ->", props)
+
+return {
+    <View style=(StyleSheet.container)
+}
+}
